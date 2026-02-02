d9d67faa06c28f322a4d0e2d377874306d033370
Jawath <mjawath@gmail.com>
2026-02-02 11:56:34 +0800

    sdgsgs

---
+++ src/main/java/com/example/testejb/TestejbApplication.java
@@ -0,0 +1,11 @@
+package com.example.testejb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestejbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestejbApplication.class, args);
+    }
+}
